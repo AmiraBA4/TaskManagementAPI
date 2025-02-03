b081da27ba71dbdb7bbd2be87f1f3a346facca83
AmiraBA4 <amirabenahmed47@gmail.com>
2025-02-03 22:57:52 +0100

    Add project files.

---
+++ WebApplication1/Controllers/TaskController.cs
@@ -0,0 +1,107 @@
+using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Data;
+using TaskManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TaskController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Retrieve  Task list 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskModel>>> GetTasks()
+        {
+            return await _context.Tasks.Include(t => t.User).ToListAsync();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskModel>> GetTaskById(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return task;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TaskModel>> CreateTask(TaskModel task)
+        {
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTasks), new { id = task.Id }, task);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTask(int id, TaskModel updatedTask)
+        {
+            if (id != updatedTask.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(updatedTask).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        private bool TaskExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
+
+    }
+
+}
+
+
+
---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Models;
+
+namespace TaskManagement.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<TaskModel> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Données de test
+            modelBuilder.Entity<User>().HasData(
+
+                new User { Id = 1, Name = "Aurelien", Email = "Aurelien.test@example.com" },
+                new User { Id = 2, Name = "Julien", Email = "Julien.test@example.com" },
+                new User { Id = 3, Name = "Emilie", Email = "Emilie.test@example.com" },
+                new User { Id = 4, Name = "Amira", Email = "Amira.test@example.com" }
+
+
+
+            );
+
+            modelBuilder.Entity<TaskModel>().HasData(
+                new TaskModel { Id = 1, Title = "Test Task1", Description = "Test In-Memory Task1", DueDate = DateTime.Now.AddDays(5), IsCompleted = false, UserId = 1 },
+                new TaskModel { Id = 2, Title = "Test Task2", Description = "Test In-Memory Task2", DueDate = DateTime.Now.AddDays(5), IsCompleted = false, UserId = 2 },
+                new TaskModel { Id = 3, Title = "Test Task3", Description = "Test In-Memory Task3", DueDate = DateTime.Now.AddDays(5), IsCompleted = false, UserId = 3 },
+                new TaskModel { Id = 4, Title = "Test Task4", Description = "Test In-Memory Task4", DueDate = DateTime.Now.AddDays(5), IsCompleted = false, UserId = 4 }
+
+            );
+        }
+    }
+}
+
